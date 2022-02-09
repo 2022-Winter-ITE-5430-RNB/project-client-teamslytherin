763b4ad63950a13df0c51ffc053aaebd8829e6ef
preetikshirsagar <preetikshirsagar93@gmail.com>
2022-02-09 15:59:03 -0500

    Adding header and footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Navbar bg="light"  expand="lg" fixed="bottom" >
+    <Container >
+    <div className='header'> Ado-pet website</div>
+    
+    </Container>
+  </Navbar>
+  )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { NavDropdown } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
+import { FormControl } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+
+
+const Header = () => {
+  return (
+    
+    <Navbar bg="light"  expand="lg" fixed="top" >
+    <Container >
+    
+    <Nav className='header'>
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">About Us</Nav.Link>
+      <Nav.Link href="#contact">Contact Us</Nav.Link>   
+    
+    <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search Pets"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+      </Nav>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default Header
