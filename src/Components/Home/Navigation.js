7eec8d8443dedea607fa8bf54e34cc9c98b3aaf5
Srinath Patnam <N01495266@humbermail.ca>
2022-02-14 22:44:36 -0500

    Create New

--- src/Components/Home/Navigation.js
+++ src/Components/Home/Navigation.js
@@ -17,7 +17,7 @@ const Navigation = () => {
           <Link to='FAQS'>FAQS</Link>
         </li>
         <li>
-          <Link to='Creat'>Create New</Link>
+          <Link to='Create'>Create New</Link>
         </li>
       </ul>
     </nav>
