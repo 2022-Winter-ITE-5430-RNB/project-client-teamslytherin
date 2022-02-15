fb38bbc611403afe60b75050a985c0c1299f1f94
Srinath Patnam <N01495266@humbermail.ca>
2022-02-14 22:57:43 -0500

    About, Events and Raise

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Events from './Events/Events'
+import About from './About/About'
+import Raise from './Raise/Raise'
 import FAQS from './FAQ/FAQS'
 import FileNotFound from './Home/FileNotFound'
 import Footer from './Home/Footer'
@@ -15,6 +18,9 @@ const Header = () => {
         <Route path='/' element={<Home />} />        
         <Route path='FAQS' element={<FAQS />} />
         <Route path='Create' element={<AdoptRequest />} />
+        <Route path='Events' element={<Events/>} />
+        <Route path='About' element={<About />} />
+        <Route path='Raise' element={<Raise />} />
         <Route path='*' element={<FileNotFound />} />
       </Routes>
       <Footer />    
--- src/Components/Home/Navigation.js
+++ src/Components/Home/Navigation.js
@@ -6,13 +6,7 @@ const Navigation = () => {
       <ul>
         <li>
           <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='About'>About</Link>
-        </li>
-        <li>
-          <Link to='Contact'>Contact Us</Link>
-        </li>
+        </li>    
         <li>
           <Link to='FAQS'>FAQS</Link>
         </li>
@@ -20,13 +14,16 @@ const Navigation = () => {
           <Link to='Create'>Create New</Link>
           </li>
           <li>
-          <Link to='events'>Events</Link>
+          <Link to='Events'>Events</Link>
         </li>
         <li>
-          <Link to='about'>About</Link>
+          <Link to='About'>About</Link>
         </li>
         <li>
-          <Link to='raise'>Raise</Link>
+          <Link to='Raise'>Raise</Link>
+        </li>
+        <li>
+          <Link to='Contact'>Contact Us</Link>
         </li>
       </ul>
     </nav>
